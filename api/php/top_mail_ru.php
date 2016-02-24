9ca7e79ac86a57883a4ab585370a745172dd1056
AloneCoder <popov.max.31337@gmail.com>
2016-02-24 23:21:08 +0300

    Some improvements
    Make php part PSR-1 compatible
    Make some small improvements

--- api/php/top_mail_ru.php
+++ api/php/top_mail_ru.php
@@ -1,98 +1,184 @@
 <?php
 
+/**
+ * Class TopMailRu
+ */
 class TopMailRu
 {
-  private $apikey;
-  private $returnArray;
-  private $session;
+    const BASE_URL = 'http://top.mail.ru';
 
-  public function __construct($apikey, $returnArray) {
-    $this->apikey = $apikey;
-    $this->returnArray = $returnArray;
-  }
-  private function bitxor($o1, $o2) {
-    $res = '';
-    $runs = strlen($o1);
-    for($i=0; $i<$runs; $i++)
-        $res .= $o1[$i] ^ $o2[$i];
-    return $res;
-  }
-  private function request($path, $argsArray, $returnArray) {
+    private $apiKey;
+    private $returnAsArray;
+    private $session;
 
-    $url = 'http://top.mail.ru' . $path . '?' . http_build_query($argsArray);
-
-    $ch = curl_init();
-//    curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
-//    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/x-www-form-urlencoded'));
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    try {
-      $data = curl_exec($ch);
-      curl_close($ch);
-//print($data);
-      $data = json_decode($data, $returnArray);
-//print_r($data);
-    } catch(Exception $e) {
-      print("\nException: " . $e->getCode() . " (" . $e->getMessage() . ")\n");
-      $data = null;
+    /**
+     * TopMailRu constructor.
+     * @param $apiKey
+     * @param $returnAsArray
+     */
+    public function __construct($apiKey, $returnAsArray)
+    {
+        $this->apiKey = $apiKey;
+        $this->returnAsArray = $returnAsArray;
     }
-    return $data;
-  }
 
-  public function registerSite($args) {
-    if ($this->apikey) $args['apikey'] = $this->apikey;
-    return $this->request('/json/add', $args, $this->returnArray);
-  }
-  public function editSite($id, $password, $args) {
-    if ($this->apikey) $args['apikey'] = $this->apikey;
-    if ($this->session) $args['session'] = $this->session;
-    $args['id'] = $id;
-    $args['password'] = $password;
-    return $this->request('/json/edit', $args, $this->returnArray);
-  }
-  public function getCode($id, $password, $args) {
-    if ($this->apikey) $args['apikey'] = $this->apikey;
-    if ($this->session) $args['session'] = $this->session;
-    $args['id'] = $id;
-    $args['password'] = $password;
-    return $this->request('/json/code', $args, $this->returnArray);
-  }
-
-  public function setSession($session) {
-    if ($session) $this->session = $session;
-  }
-  public function login($id, $password) {
-    $args = array('id' => $id, 'password' => $password, 'action' => 'json');
-    if ($this->apikey) $args['apikey'] = $this->apikey;
-    if ($this->session) $args['session'] = $this->session;
-    $res = $this->request('/json/login', $args, true);
-    if ($res['session']) $this->session = $res['session'];
-    return (isset($res['logged']) && $res['logged'] == 'yes');
-  }
-  public function loginByHash($id, $ph) {
-    $args = array('id' => $id, 'action' => 'json');
-    if ($this->apikey) $args['apikey'] = $this->apikey;
-    if ($this->session) $args['session'] = $this->session;
-    $res = $this->request('/json/login', $args, true);
-    if ($res['session']) $this->session = $res['session'];
-    if (isset($res['salt'], $res['seed'])) {
-      $hash2 = sha1($res['salt'] . $ph, true);
-      $hash3 = $this->bitxor($ph, sha1($res['seed'] . $hash2, true));
-      $args = array('id' => $id, 'action' => 'json', 'ph' => bin2hex($hash3), 'seed' => $res['seed']);
-      if ($this->apikey) $args['apikey'] = $this->apikey;
-      if ($this->session) $args['session'] = $this->session;
-      $res = $this->request('/json/login', $args, true);
+    /**
+     * @param $o1
+     * @param $o2
+     * @return string
+     */
+    protected function bitxor($o1, $o2)
+    {
+        $result = '';
+        $runs = strlen($o1);
+        for ($i = 0; $i < $runs; $i++)
+            $result .= $o1[$i] ^ $o2[$i];
+        return $result;
     }
-    return (isset($res['logged']) && $res['logged'] == 'yes');
-  }
 
-  public function getStat($id, $password, $type, $args) {
-    if ($this->apikey) $args['apikey'] = $this->apikey;
-    if ($this->session) $args['session'] = $this->session;
-    $args['id'] = $id;
-    $args['password'] = $password;
-    return $this->request('/json/' . $type, $args, $this->returnArray);
-  }
+    /**
+     * @param $path
+     * @param $argsArray
+     * @param $returnAsArray
+     * @return mixed|null
+     */
+    protected function request($path, $argsArray, $returnAsArray)
+    {
+        $url = static::BASE_URL . $path . '?' . http_build_query($argsArray);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = null;
+        try {
+            $data = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($data, $returnAsArray);
+        } catch (Exception $e) {
+            echo "Exception: {$e->getCode()} ({$e->getMessage()})", PHP_EOL;
+        }
 
+        return $data;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getKeyAndSession()
+    {
+        $args = array();
+        if ($this->apiKey) {
+            $args['apikey'] = $this->apiKey;
+        }
+        if ($this->session) {
+            $args['session'] = $this->session;
+        }
+        return $args;
+    }
+
+    /**
+     * @param $args
+     * @return mixed|null
+     */
+    public function registerSite($args)
+    {
+        $args += $this->getKeyAndSession();
+        return $this->request('/json/add', $args, $this->returnAsArray);
+    }
+
+    /**
+     * @param $id
+     * @param $password
+     * @param $args
+     * @return mixed|null
+     */
+    public function editSite($id, $password, $args)
+    {
+        $args += $this->getKeyAndSession();
+        $args['id'] = $id;
+        $args['password'] = $password;
+        return $this->request('/json/edit', $args, $this->returnAsArray);
+    }
+
+    /**
+     * @param $id
+     * @param $password
+     * @param $args
+     * @return mixed|null
+     */
+    public function getCode($id, $password, $args)
+    {
+        $args += $this->getKeyAndSession();
+        $args['id'] = $id;
+        $args['password'] = $password;
+        return $this->request('/json/code', $args, $this->returnAsArray);
+    }
+
+    /**
+     * @param $session
+     */
+    public function setSession($session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param $id
+     * @param $password
+     * @return bool
+     */
+    public function login($id, $password)
+    {
+        $args = array_merge(
+            array('id' => $id, 'password' => $password, 'action' => 'json'),
+            $this->getKeyAndSession()
+        );
+        $res = $this->request('/json/login', $args, true);
+        if ($res['session']) {
+            $this->session = $res['session'];
+        }
+        return isset($res['logged']) && $res['logged'] === 'yes';
+    }
+
+    /**
+     * @param $id
+     * @param $ph
+     * @return bool
+     */
+    public function loginByHash($id, $ph)
+    {
+        $args = array_merge(
+            array('id' => $id, 'action' => 'json'),
+            $this->getKeyAndSession()
+        );
+        $response = $this->request('/json/login', $args, true);
+        if ($response['session']) {
+            $this->session = $response['session'];
+        }
+        if (isset($response['salt'], $response['seed'])) {
+            $hash2 = sha1($response['salt'] . $ph, true);
+            $hash3 = $this->bitxor($ph, sha1($response['seed'] . $hash2, true));
+            $args = array_merge(
+                array('id' => $id, 'action' => 'json', 'ph' => bin2hex($hash3), 'seed' => $response['seed']),
+                $this->getKeyAndSession()
+            );
+            $response = $this->request('/json/login', $args, true);
+        }
+
+        return (isset($response['logged']) && $response['logged'] === 'yes');
+    }
+
+    /**
+     * @param $id
+     * @param $password
+     * @param $type
+     * @param $args
+     * @return mixed|null
+     */
+    public function getStat($id, $password, $type, $args)
+    {
+        $args += $this->getKeyAndSession();
+        $args['id'] = $id;
+        $args['password'] = $password;
+        return $this->request('/json/' . $type, $args, $this->returnAsArray);
+    }
 }
